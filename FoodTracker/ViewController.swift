2fac3b82ce2b6d8a8b6d8b67e80061af075564f9
Baris Soybas <barissoybas@me.com>
2019-01-03 13:57:17 +0100

    aq

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -5,7 +5,7 @@
 //  Created by Jane Appleseed on 10/17/16.
 //  Copyright © 2016 Apple Inc. All rights reserved.
 //
-// baris3.0 yeter mk
+// baris3.0 yeter mk 4.0
 // murat1.1.1.1..1.1.1.1.1.1.
 
 import UIKit
